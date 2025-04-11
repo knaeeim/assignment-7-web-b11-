f7d78ded5c944a11c37aedc15ad5f3acba3351a4
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2025-04-11 18:27:39 +0530

    Fixed height issue

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className='relative'>
             <div>
-                <img className='w-full h-[85vh] object-cover' src='assets/Banner-min.jpg'></img>
+                <img className='w-full md:h-[85vh] h-[60vh] object-cover' src='assets/Banner-min.jpg'></img>
             </div>
             <div className='absolute md:top-1/6 top-1/8 md:left-1/9 left-1/12 transform translate-x-3 translate-y-1/5'>
                 <h1 className='md:text-6xl text-xl font-bold md:font-normal text-white'>Bid on Unique items from <br /> Around the world!!</h1>
